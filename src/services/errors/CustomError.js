c647582b2ad35a4c2a6d26ff866d8fdd7ee3e15e
Fernando Marcos Giraudo <fernando.giraudo@intive.com>
2023-08-23 10:35:15 -0300

    correction added

--- src/services/errors/CustomError.js
+++ src/services/errors/CustomError.js
@@ -1,5 +1,7 @@
 export default class CustomError {
     static createError({ name= "Error", cause, message, code = 1}){
+        console.log("HOLA desde customError");
+        return;
         const error = new Error(message);
         error.cause = cause;
         error.name = name;
